491e21d2298e3c09d6c6bc9b5f28fcbb7a8203e3
Esdras Malave <esdras.malave.04@gmail.com>
2024-08-01 21:14:43 -0400

    Se añadio seccion principal de ejemplo

---
+++ src/components/EjemploPublicacion.vue
@@ -0,0 +1,110 @@
+<template>
+  <section>
+    <div class="my-4">
+      <h2 class="my-4">Ejemplo de publicación</h2>
+    </div>
+
+    <section class="vista-previa">
+      <p>
+        Saludos querida red 👋 en #Hooli tenemos el agrado de informar sobre
+        nuestra búsqueda de nuevo talento para el cargo #DesarrolladorFrontend
+        🚀 en modalidad #Hibrida.
+      </p>
+      <br />
+
+      <h3>🏢 Sobre la empresa: Hooli</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus
+        suscipit mauris eu molestie. Nulla ultrices feugiat mauris. In hac
+        habitasse platea dictumst. Nam sagittis eros est, et elementum neque
+        fringilla vitae. Quisque ultricies sollicitudin tortor, id ultrices
+        augue pellentesque a.
+      </p>
+      <br />
+
+      <h3>💼 Sobre la vacante: Desarrollador Frontend</h3>
+      <p>
+        Vestibulum diam erat, sodales finibus nisi eget, interdum sagittis nibh.
+        Ut vulputate massa ligula, nec semper odio laoreet vitae. Duis rutrum,
+        dolor ac consequat finibus, arcu ex viverra lacus, sit amet blandit
+        metus odio a risus. Nullam erat lorem, placerat ultrices volutpat ut,
+        commodo sed est.
+      </p>
+      <br />
+
+      <h3>📋 Responsabilidades</h3>
+      <ul>
+        <li>🔹 Lorem ipsum dolor sit amet</li>
+        <li>🔹 Nullam elementum ultricies sem</li>
+        <li>🔹 Nullam viverra at orci sed feugiat</li>
+        <li>🔹 Phasellus elementum nulla</li>
+        <li>🔹 Mauris eget est efficitur</li>
+      </ul>
+      <br />
+
+      <h3>✅ Requisitos excluyentes</h3>
+      <ul>
+        <li>🔹 HTML</li>
+        <li>🔹 CSS</li>
+        <li>🔹 JavaScript</li>
+      </ul>
+      <br />
+
+      <h3>⭐ Requisitos deseables</h3>
+      <ul>
+        <li>🔹 Vue.js</li>
+        <li>🔹 Vuetify</li>
+        <li>🔹 TypeScript</li>
+      </ul>
+      <br />
+
+      <h3>💰 Salario</h3>
+      <p>Entre 960.000 y 1.200.000 CLP</p>
+      <br />
+
+      <h3>🎁 Beneficios</h3>
+      <ul>
+        <li>🔹 Phasellus</li>
+        <li>🔹 Fusce</li>
+        <li>🔹 Lorem ipsum</li>
+        <li>🔹 Mauris eget</li>
+        <li>🔹 Morbi blandit</li>
+      </ul>
+      <br />
+
+      <h3>🕒 Jornada: Temporal</h3>
+      <h3>🌐 Modalidad: Hibrida</h3>
+      <h3>🌎 País: Chile</h3>
+      <h3>📍Dirección: Av. Pdte. Kennedy 46654, Las Condes, RM</h3>
+      <br />
+
+      <h3>✉️ Contacto: usuario@info.com</h3>
+      <br />
+
+      <p>
+        Si nuestra vacante se ajusta a tu perfil, no dudes en enviarnos tu CV
+        actualizado al correo de contacto con el asunto
+        <strong>Desarrollador Frontend</strong>, junto con un breve resumen
+        sobre tí.
+      </p>
+      <br />
+
+      <strong>
+        #Hooli #DesarrolladorFrontend #HTML #CSS #JavaScript #TrabajoTemporal
+        #ModalidadHibrida #Chile #SimpliPost
+      </strong>
+    </section>
+  </section>
+</template>
+
+<style scoped>
+.vista-previa {
+  background-color: rgb(var(--v-theme-surface-light));
+  border-radius: 8px;
+  padding: 16px 36px;
+}
+
+ul {
+  list-style-type: none;
+}
+</style>
